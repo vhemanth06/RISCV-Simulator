a7e1432ed15d00e06d86576020deb420dd7d1041
hemanth804 <vhemanthreddy06@gmail.com>
2024-09-24 16:13:08 +0530

    .data version

--- main.c
+++ main.c
@@ -3,13 +3,20 @@
 #include <stdlib.h>
 #include "functions.h"
 #include "run.h"
+#include <stdint.h>
 
 #define MAX_TOKENS 10
 #define MAX_INPUT_SIZE 100 
+long int default_register_value[]={0,0,0,0,0,0,0,0,
+                      0,0,0,0,0,0,0,0,
+                      0,0,0,0,0,0,0,0,
+                      0,0,0,0,0,0,0,0,};
 long int register_value[]={0,0,0,0,0,0,0,0,
                       0,0,0,0,0,0,0,0,
                       0,0,0,0,0,0,0,0,
                       0,0,0,0,0,0,0,0,};
+uint64_t mem_address[50000];
+long int mem_values[50000];
 
 int main() {
     char command[MAX_INPUT_SIZE]; 
@@ -21,6 +28,7 @@ int main() {
            if(strcmp(tokens_comm[0], "load") == 0){
             //printf("%s\n",tokens_comm[0]);
             input = fopen(tokens_comm[1], "r");
+            memcpy(register_value,default_register_value,sizeof(register_value));
             //printf("runcheck1\n");
             if(input==NULL){
                 printf("error opening file\n");
@@ -35,10 +43,49 @@ int main() {
                 //printf("runcheck2\n");
                 char line[MAX_INPUT_SIZE];
                 int instr_counter=1;
+                char *array_of_lines[60];
+
+                uint64_t d_address=0x10000;
                 while(fgets(line,sizeof(line),input)!=NULL){
+                     
+                }
+                while(fgets(line,sizeof(line),input)!=NULL){
+                    if(strcmp(line,".data")==0){
+                        continue;
+                    }
+                    if(strcmp(line,".text")==0){
+                        break;
+                    }
+                    if(line != NULL){
+                        for (char *p = line; *p; p++) {
+                            if (*p == ',') *p = ' ';
+                        }    
+                    }
+
+                    if (line == NULL) continue;
+                    char **data=string_split(line);
+                    if(strcmp(data,".dword")==0){
+
+                    } else if(strcmp(data,".word")==0){
+
+                    } else if(strcmp(data,".half")==0){
+
+                    } else if(strcmp(data,".byte")==0){
+
+                    } 
+                }
+                while(fgets(line,sizeof(line),input)!=NULL){
+                    int size=strlen(line);
+                    if(strcmp(line,".data")==0 || strcmp(line,".text")==0){
+                        continue;
+                    }
+                    // if(line[0]=='.'){
+                    //     char line_copy2[size];
+                    //     strcpy(line_copy2,line);
+                    // }
                     char *instruction;
                     char *label;
-                    int size=strlen(line);
+                    
                     char line_copy1[size];
                     strcpy(line_copy1,line);
                     char *tokens_for_labels = strtok(line, ":\n\r");
@@ -59,7 +106,10 @@ int main() {
                     }
                     if (instruction == NULL) continue;
                     char **tokens = string_split(instruction);
-                    run_instruction(tokens, register_value);
+                    if(strcmp(tokens[0],".dword")==0 ||strcmp(tokens[0],".word")==0 ||strcmp(tokens[0],".half")==0 ||strcmp(tokens[0],".byte")==0){
+                         continue;
+                    }
+                    run_instruction(tokens, register_value,mem_address,mem_values);
                 }
                 
            } else if(strcmp(tokens_comm[0],"reg")==0){
--- run.c
+++ run.c
@@ -1,15 +1,16 @@
 #include "functions.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
-void run_instruction(char **tokens, long int register_value[]){
+void run_instruction(char **tokens, long int register_value[],uint64_t mem_address[],long int mem_values[]){
     if (strcmp(tokens[0], "add") == 0) {
         int rd = register_finder(tokens[1]);
         int rs1 = register_finder(tokens[2]);
         int rs2 = register_finder(tokens[3]);
 
         register_value[rd] = register_value[rs1] +  register_value[rs2];
-        printf("%d+%d=%d\n",register_value[rs1],register_value[rs2],register_value[rd]);
+        //printf("%d+%d=%d\n",register_value[rs1],register_value[rs2],register_value[rd]);
             
     } else if (strcmp(tokens[0], "sub") == 0) {
         int rd = register_finder(tokens[1]);
@@ -128,6 +129,24 @@ void run_instruction(char **tokens, long int register_value[]){
 
         if (num <= 63 && num >= 0){
             register_value[rd] = register_value[rs1] >> num;
+        }
+            
+    } else if (strcmp(tokens[0], "ld") == 0) {
+        int rd = register_finder(tokens[1]);
+        int rs1 = register_finder(tokens[3]);
+        int num = atoi(tokens[2]);
+
+        if (num <= 1023 && num >= 1024){
+        long int address = num+register_value[rs1];
+        for(int i=0;i<sizeof(mem_address);i++){
+            if(address==mem_address[i]){
+                register_value[rd]=mem_values[i];
+                break;
+            } else {
+                register_value[rd]=0;
+            }
+        }
+
         }
             
     }
